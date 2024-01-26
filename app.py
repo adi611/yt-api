eca5f524709b9a7340e7a91ae2ac01529935ebda
Aditya Agarwal <adityaagarwal@Adityas-MacBook-Pro.local>
2024-01-26 11:09:29 +0530

    handle multiple api keys

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 # import modules
+from datetime import datetime
 from flask import Flask, request, jsonify
-import requests
 import sqlite3
-import time
 import threading
 from dotenv import load_dotenv
 import os
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,15 +15,19 @@ load_dotenv()
 app = Flask(__name__)
 
 # Define constants
-YOUTUBE_API_KEY = os.getenv(
-    "YOUTUBE_API_KEY"
-)  # Retrieve YouTube API key from environment variable
+YOUTUBE_API_KEYS = os.getenv("YOUTUBE_API_KEY").split(
+    ","
+)  # Retrieve YouTube API keys from environment variable
 YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3"
 DB_NAME = "videos.db"
 TABLE_NAME = "videos"
 TABLE_SCHEMA = "(video_id TEXT PRIMARY KEY, title TEXT, description TEXT, published_at TEXT, thumbnail_url TEXT)"
 SEARCH_QUERY = "cricket"
 INTERVAL = 10  # seconds
+
+# Global variables
+current_api_key_index = 0
+
 
 # Database utility functions
 def create_table_if_not_exists():
@@ -32,6 +37,7 @@ def create_table_if_not_exists():
     conn.commit()
     conn.close()
 
+
 def insert_video_data(video_data):
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
@@ -39,22 +45,41 @@ def insert_video_data(video_data):
     conn.commit()
     conn.close()
 
+
 def fetch_videos_from_youtube():
     print('in fetch_videos_from_youtube...')
-    current_time = time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
-    search_url = YOUTUBE_BASE_URL + "/search"
-    search_params = {
-        "key": YOUTUBE_API_KEY,
-        "part": "snippet",
-        "type": "video",
-        "order": "date",
-        "publishedAfter": current_time,
-        "q": SEARCH_QUERY
-    }
-    response = requests.get(search_url, params=search_params)
-    if response.status_code == 200:
-        data = response.json()
-        for item in data["items"]:
+    api_key_index = 0
+    valid = False
+    last_request_time = datetime.utcnow()
+
+    while not valid and api_key_index < len(YOUTUBE_API_KEYS):
+        try:
+            api_key = YOUTUBE_API_KEYS[api_key_index]
+            youtube = build("youtube", "v3", developerKey=api_key)
+            req = youtube.search().list(
+                q=SEARCH_QUERY,
+                part="snippet",
+                order="date",
+                maxResults=50,
+                publishedAfter=(
+                    last_request_time.replace(microsecond=0).isoformat() + "Z"
+                ),
+            )
+            res = req.execute()
+            print(res)
+            valid = True
+        except HttpError as err:
+            print(err)
+            code = err.resp.status
+            if not (code == 400 or code == 403):
+                break
+            else:
+                print('api_key_index updated...')
+                api_key_index += 1
+
+    if valid:
+        # Process the response and store in the database
+        for item in res["items"]:
             video_id = item["id"]["videoId"]
             snippet = item["snippet"]
             title = snippet["title"]
@@ -64,11 +89,13 @@ def fetch_videos_from_youtube():
             video_data = (video_id, title, description, published_at, thumbnail_url)
             insert_video_data(video_data)
 
+
 # Scheduler utility function
 def schedule_fetch_and_store_videos():
-    print('in schedule_fetch_and_store_videos...')
+    print("in schedule_fetch_and_store_videos...")
     threading.Timer(INTERVAL, schedule_fetch_and_store_videos).start()
     fetch_videos_from_youtube()
+
 
 # Routes
 @app.route("/videos", methods=["GET"])
@@ -79,11 +106,23 @@ def get_videos():
     limit = per_page
     conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
-    cursor.execute(f"SELECT * FROM {TABLE_NAME} ORDER BY published_at DESC LIMIT ? OFFSET ?", (limit, offset))
+    cursor.execute(
+        f"SELECT * FROM {TABLE_NAME} ORDER BY published_at DESC LIMIT ? OFFSET ?",
+        (limit, offset),
+    )
     rows = cursor.fetchall()
     conn.close()
-    video_data = [dict(zip(["video_id", "title", "description", "published_at", "thumbnail_url"], row)) for row in rows]
+    video_data = [
+        dict(
+            zip(
+                ["video_id", "title", "description", "published_at", "thumbnail_url"],
+                row,
+            )
+        )
+        for row in rows
+    ]
     return jsonify(video_data)
+
 
 # Initialize database and start scheduler
 create_table_if_not_exists()
